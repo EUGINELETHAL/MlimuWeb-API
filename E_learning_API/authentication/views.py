1b865921dec09086de3bda82a96b8b0c5acb25f8
EUGINELETHAL <ochungeugine@gmail.com>
2020-06-13 03:32:48 +0300

    created teacher registeration

--- E_learning_API/authentication/views.py
+++ E_learning_API/authentication/views.py
@@ -1,7 +1,7 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import User
-from .serializers import UserSerializer, LoginSerializer,StudentSerializer
+from .serializers import*
 from rest_framework import status
 from django.contrib.auth import authenticate
 
@@ -27,6 +27,22 @@ class UserCreate(APIView):
 
 class StudentRegisterView(APIView):
     serializer_class = StudentSerializer
+    
+    def post(self, request, format=None):
+        data = request.data
+
+        # Notice here that we do not call `serializer.save()` like we did for
+        # the registration endpoint. This is because we don't  have
+        # anything to save. Instead, the `validate` method on our serializer
+        # handles everything we need.
+        serializer = self.serializer_class(data=data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+class TeacherRegisterView(APIView):
+    serializer_class = TeacherSerializer
     
     def post(self, request, format=None):
         data = request.data
